6861c559c01710cb484bb77227fe65c9013b6fb4
Francisco Cardoso <fjcardoso@Franciscos-MBP.lan>
2019-12-11 15:20:01 +0000

    v2

--- Client.java
+++ Client.java
@@ -1,16 +1,12 @@
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
 import java.net.Socket;
 import java.security.*;
 import java.security.cert.CertificateException;
@@ -29,15 +25,19 @@ public class Client {
     public static final String RECEIVE = "recv";
     public static final String RECEIPT = "receipt";
     public static final String STATUS = "status";
+    public static final String LIST = "list";
+    public static final String NEW = "new";
 
     private static final String cipherMode = "RSA/None/NoPadding";
     private static Socket socket;
-    private static Cipher cipher;
+    private static Cipher cipher1, cipher2;
+    private static SecretKey secretKey;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchProviderException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, IllegalBlockSizeException, NoSuchPaddingException, CertificateException, SignatureException, KeyStoreException, ParseException, UnrecoverableKeyException {
         socket = new Socket("localhost", 9000);
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-        cipher = Cipher.getInstance(cipherMode, "BC");
+        cipher1 = Cipher.getInstance("RSA/None/NoPadding", "BC");
+        cipher2 = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
 
         Scanner in = new Scanner(System.in);
         String command = (in.nextLine()).toLowerCase();
@@ -46,7 +46,10 @@ public class Client {
                 String[] cmd = command.split(" ");
                 switch (cmd[0]) {
                     case ALL:
-                        function(cmd[0], Integer.parseInt(cmd[1]));
+                        all(cmd[0], Integer.parseInt(cmd[1]));
+                        break;
+                    case NEW:
+                        newMsgs(cmd[0], Integer.parseInt(cmd[1]));
                         break;
                     case CREATE: ;
                         create(cmd[0], Integer.parseInt(cmd[1]));
@@ -63,6 +66,9 @@ public class Client {
                     case STATUS: ;
                         status(cmd[0], Integer.parseInt(cmd[1]), cmd[2]);
                         break;
+                    case LIST: ;
+                        list(cmd[0]);
+                        break;
                     case EXIT:
                         System.exit(1);
                     default:
@@ -73,7 +79,20 @@ public class Client {
         }
     }
 
-    private static void function(String type, int user) throws IOException {
+    private static void all(String type, int user) throws IOException {
+        JsonWriter wrt = new JsonWriter(new OutputStreamWriter(socket.getOutputStream()));
+        wrt.beginObject();
+        wrt.name("type").value(type);
+        wrt.name("id").value(user);
+        wrt.endObject();
+        wrt.flush();
+        JsonReader js = new JsonReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
+        JsonElement data = new JsonParser().parse(js);
+        if (data.isJsonObject())
+            System.out.println(data.getAsJsonObject());
+    }
+
+    private static void newMsgs(String type, int user) throws IOException {
         JsonWriter wrt = new JsonWriter(new OutputStreamWriter(socket.getOutputStream()));
         wrt.beginObject();
         wrt.name("type").value(type);
@@ -102,6 +121,22 @@ public class Client {
 
     private static void send(String type, int sender, int receiver, String msg, String copy) throws IOException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException, NoSuchProviderException, SignatureException {
 
+        //Generate session key
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES", "BC");
+        keyGen.init(256); // for example
+        secretKey = keyGen.generateKey();
+
+        //Cifrar com chave de sessao
+        cipher2.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] sessionEncrypted = cipher2.doFinal(msg.getBytes());
+
+        //Append key to msg
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        outputStream.write( sessionEncrypted );
+        outputStream.write( secretKey.getEncoded() );
+
+        byte sessionAndkey[] = outputStream.toByteArray( );
+
         KeyStore keyStore = KeyStore.getInstance("JKS");
         FileInputStream stream = new FileInputStream("server.jks");
         keyStore.load(stream, "password".toCharArray());
@@ -111,19 +146,25 @@ public class Client {
         //Veryify rootCA certificate
         user2[0].verify(user2[1].getPublicKey());
 
-        // Cifrar
-        cipher.init(Cipher.ENCRYPT_MODE, user2[0].getPublicKey());
+        // Cifrar com chave publica do recetor, assinatura digital
+        cipher1.init(Cipher.ENCRYPT_MODE, user2[0].getPublicKey());
 
-        byte[] cipherText = cipher.doFinal(msg.getBytes());
+        byte[] cipherText = cipher1.doFinal(sessionAndkey);
+        JsonObject obj = new JsonObject();
+        obj.addProperty("content", Base64.getEncoder().encodeToString(cipherText));
+        obj.addProperty("size", sessionEncrypted.length);
+        obj.addProperty("alg", "AES/ECB/PKCS5Padding");
+
         JsonWriter wrt = new JsonWriter(new OutputStreamWriter(socket.getOutputStream()));
         wrt.beginObject();
         wrt.name("type").value(type);
         wrt.name("src").value(sender);
         wrt.name("dst").value(receiver);
-        wrt.name("msg").value(Base64.getEncoder().encodeToString(cipherText));
+        wrt.name("msg").value(Base64.getEncoder().encodeToString(obj.toString().getBytes()));
         wrt.name("copy").value(copy);
         wrt.endObject();
         wrt.flush();
+
         JsonReader js = new JsonReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
         JsonElement data = new JsonParser().parse(js);
         if (data.isJsonObject())
@@ -145,12 +186,29 @@ public class Client {
         wrt.name("msg").value(msgId);
         wrt.endObject();
         wrt.flush();
+
         JsonReader js = new JsonReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
         JsonElement data = new JsonParser().parse(js);
-        cipher.init(Cipher.DECRYPT_MODE, privKey);
-        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(data.getAsJsonObject().get("result").getAsJsonArray().get(1).getAsString()));
-        if (data.isJsonObject())
-            System.out.println(new String(decrypted));
+        cipher1.init(Cipher.DECRYPT_MODE, privKey);
+        //byte[] authenticated = cipher1.doFinal(Base64.getDecoder().decode(data.getAsJsonObject().get("result").getAsJsonArray().get(1).getAsString()));
+        String x = new String(Base64.getDecoder().decode(data.getAsJsonObject().get("result").getAsJsonArray().get(1).getAsString()));
+
+        JsonObject jsonObject = new JsonParser().parse(x).getAsJsonObject();
+
+        byte[] cipherText = Base64.getDecoder().decode(jsonObject.get("content").getAsString());
+        byte[] sessionAndKey = cipher1.doFinal(cipherText);
+
+        byte[] msg = new byte[jsonObject.get("size").getAsInt()];
+        byte[] key = new byte[sessionAndKey.length - jsonObject.get("size").getAsInt()];
+        System.arraycopy(sessionAndKey,0,msg,0,jsonObject.get("size").getAsInt());
+        System.arraycopy(sessionAndKey,jsonObject.get("size").getAsInt(),key,0,sessionAndKey.length - jsonObject.get("size").getAsInt());
+
+        SecretKey originalKey = new SecretKeySpec(key, 0, key.length, "AES");
+
+
+       cipher2.init(Cipher.DECRYPT_MODE, originalKey);
+       byte[] decrypted = cipher2.doFinal(msg);
+       System.out.println(new String(decrypted));
 
 
     }
@@ -178,6 +236,18 @@ public class Client {
         wrt.name("id").value(id);
         wrt.name("msg").value(msgId);
         wrt.name("receipt").value(receipt);
+        wrt.endObject();
+        wrt.flush();
+        //JsonReader js = new JsonReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
+        //JsonElement data = new JsonParser().parse(js);
+        //if (data.isJsonObject())
+        //    System.out.println(data.getAsJsonObject());
+    }
+
+    private static void list(String type) throws IOException {
+        JsonWriter wrt = new JsonWriter(new OutputStreamWriter(socket.getOutputStream()));
+        wrt.beginObject();
+        wrt.name("type").value(type);
         wrt.endObject();
         wrt.flush();
         //JsonReader js = new JsonReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
