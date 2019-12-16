5f8aeca3b9ae7a911c5b186665affb5472875643
Francisco Cardoso <fjcardoso@Franciscos-MacBook-Pro.local>
2019-12-16 18:23:57 +0000

    encrypted correctly for large msgs and receipts done

--- Client.java
+++ Client.java
@@ -29,7 +29,6 @@ public class Client {
     private static final String cipherMode = "RSA/None/NoPadding";
     private static Socket socket;
     private static Cipher authCipher1, authCipher2, sessionCipher;
-    private static SecretKey secretKey;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchProviderException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, IllegalBlockSizeException, NoSuchPaddingException, CertificateException, SignatureException, KeyStoreException, ParseException, UnrecoverableKeyException {
         socket = new Socket("localhost", 9002);
@@ -53,7 +52,7 @@ public class Client {
                         create(cmd[0], Integer.parseInt(cmd[1]));
                         break;
                     case SEND: ;
-                        send(cmd[0], Integer.parseInt(cmd[1]), Integer.parseInt(cmd[2]), cmd[3], cmd[3]);
+                        send(cmd[0], Integer.parseInt(cmd[1]), Integer.parseInt(cmd[2]), cmd[3]);
                         break;
                     case RECEIVE: ;
                         receive(cmd[0], Integer.parseInt(cmd[1]), cmd[2]);
@@ -117,29 +116,22 @@ public class Client {
             System.out.println(data.getAsJsonObject());
     }
 
-    private static void send(String type, int sender, int receiver, String msg, String copy) throws IOException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException, NoSuchProviderException, SignatureException, NoSuchPaddingException {
+    private static void send(String type, int sender, int receiver, String msg) throws IOException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, KeyStoreException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException, NoSuchProviderException, SignatureException, NoSuchPaddingException {
 
         //Generate session key
         KeyGenerator keyGen = KeyGenerator.getInstance("AES", "BC");
         keyGen.init(256); // for example
-        secretKey = keyGen.generateKey();
+        SecretKey sessionKey = keyGen.generateKey();
 
         //Cifrar com chave de sessao
         sessionCipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
-        sessionCipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        sessionCipher.init(Cipher.ENCRYPT_MODE, sessionKey);
         byte[] sessionEncrypted = sessionCipher.doFinal(msg.getBytes());
-
-        //Append key to msg
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
-        outputStream.write( sessionEncrypted );
-        outputStream.write( secretKey.getEncoded() );
-
-        byte sessionAndkey[] = outputStream.toByteArray();
 
         //Criar jsonObject com mensagem, parametros e chave
         JsonObject obj = new JsonObject();
-        obj.addProperty("content", Base64.getEncoder().encodeToString(sessionAndkey));
-        obj.addProperty("size", sessionEncrypted.length);
+        //obj.addProperty("content", Base64.getEncoder().encodeToString(sessionAndkey));
+        obj.addProperty("key", Base64.getEncoder().encodeToString(sessionKey.getEncoded()));
         obj.addProperty("alg", "AES/ECB/PKCS5Padding");
 
         String msg64 = Base64.getEncoder().encodeToString(obj.toString().getBytes());
@@ -166,14 +158,30 @@ public class Client {
         authCipher2.init(Cipher.ENCRYPT_MODE, user2[0].getPublicKey());
         byte[] authenticated = authCipher2.doFinal(auth1);
 
+        //Append key to msg
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream( );
+        outputStream.write( sessionEncrypted );
+        outputStream.write( authenticated );
+
+        byte sessionAndkey[] = outputStream.toByteArray();
+
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        sha256_HMAC.init(sessionKey);
+        byte[] copy = sha256_HMAC.doFinal(msg.getBytes());
+
+        //Criar jsonObject com mensagem, parametros e chave
+        JsonObject msgObj = new JsonObject();
+        //obj.addProperty("content", Base64.getEncoder().encodeToString(sessionAndkey));
+        msgObj.addProperty("content", Base64.getEncoder().encodeToString(sessionAndkey));
+        msgObj.addProperty("size", sessionEncrypted.length);
 
         JsonWriter wrt = new JsonWriter(new OutputStreamWriter(socket.getOutputStream()));
         wrt.beginObject();
         wrt.name("type").value(type);
         wrt.name("src").value(sender);
         wrt.name("dst").value(receiver);
-        wrt.name("msg").value(Base64.getEncoder().encodeToString(authenticated));
-        wrt.name("copy").value(copy);
+        wrt.name("msg").value(Base64.getEncoder().encodeToString(msgObj.toString().getBytes()));
+        wrt.name("copy").value(Base64.getEncoder().encodeToString(copy));
         wrt.endObject();
         wrt.flush();
 
@@ -220,43 +228,60 @@ public class Client {
         }
 
         //Get encryption parameters and key from jsonObject
-        String sParams = data.getAsJsonObject().get("result").getAsJsonArray().get(1).getAsString();
+        String msgAndParams = data.getAsJsonObject().get("result").getAsJsonArray().get(1).getAsString();
 
         //Decode JsonObject from base64
-        byte[] msgDecoded = Base64.getDecoder().decode(sParams);
+        byte[] msgDecoded = Base64.getDecoder().decode(msgAndParams);
+        System.out.println(new String(msgDecoded));
+        //Parse JsonObject with contents and params to decryption
+        JsonObject msgAndParamsObj = new JsonParser().parse(new String(msgDecoded)).getAsJsonObject();
+
+        //Decode contents from base64
+        byte[] sessionAndKey = Base64.getDecoder().decode(msgAndParamsObj.get("content").getAsString());
+
+        //Separate encrpyted message and key
+        byte[] msg = new byte[msgAndParamsObj.get("size").getAsInt()];
+        byte[] keyAndParams = new byte[sessionAndKey.length - msgAndParamsObj.get("size").getAsInt()];
+        System.arraycopy(sessionAndKey,0,msg,0,msgAndParamsObj.get("size").getAsInt());
+        System.arraycopy(sessionAndKey,msgAndParamsObj.get("size").getAsInt(),keyAndParams,0,sessionAndKey.length - msgAndParamsObj.get("size").getAsInt());
 
         //Initialize ciphers
         authCipher1.init(Cipher.DECRYPT_MODE, privKey);
         authCipher2.init(Cipher.DECRYPT_MODE, user2[0].getPublicKey());
 
         //Decrypt with private key from the receptor
-        byte[] auth1 = authCipher1.doFinal(msgDecoded);
+        byte[] auth1 = authCipher1.doFinal(keyAndParams);
 
         //Decrypt with senders public key
         byte[] sessionParams = authCipher2.doFinal(auth1);
 
+        //Decode JsonObject from base64
+        byte[] keyAndParamsDecoded = Base64.getDecoder().decode(sessionParams);
+
         //Parse JsonObject with contents and params to decryption
-        JsonObject msgAndParams = new JsonParser().parse(new String(Base64.getDecoder().decode(sessionParams))).getAsJsonObject();
+        JsonObject keyParamsObj = new JsonParser().parse(new String(keyAndParamsDecoded)).getAsJsonObject();
 
-        //Decode contents from base64
-        byte[] sessionAndKey = Base64.getDecoder().decode(msgAndParams.get("content").getAsString());
+        byte[] key = Base64.getDecoder().decode(keyParamsObj.get("key").getAsString());
 
-        //Separate encrpyted message and key
-        byte[] msg = new byte[msgAndParams.get("size").getAsInt()];
-        byte[] key = new byte[sessionAndKey.length - msgAndParams.get("size").getAsInt()];
-        System.arraycopy(sessionAndKey,0,msg,0,msgAndParams.get("size").getAsInt());
-        System.arraycopy(sessionAndKey,msgAndParams.get("size").getAsInt(),key,0,sessionAndKey.length - msgAndParams.get("size").getAsInt());
-
-        SecretKey originalKey = new SecretKeySpec(key, 0, key.length, "AES");
+        //Get session key
+        SecretKey sessionKey = new SecretKeySpec(key, 0, key.length, "AES");
 
         //Init sessionCipher
-        sessionCipher = Cipher.getInstance(msgAndParams.get("alg").getAsString(), "BC");
+        sessionCipher = Cipher.getInstance(keyParamsObj.get("alg").getAsString(), "BC");
 
         //Decrypt message with session key and print
-        sessionCipher.init(Cipher.DECRYPT_MODE, originalKey);
+        sessionCipher.init(Cipher.DECRYPT_MODE, sessionKey);
         byte[] decrypted = sessionCipher.doFinal(msg);
         System.out.println(new String(decrypted));
 
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        sha256_HMAC.init(sessionKey);
+        byte[] copy = sha256_HMAC.doFinal(decrypted);
+
+
+        receipt("receipt", id, msgId, Base64.getEncoder().encodeToString(copy));
+        //receipt("receipt", Integer.parseInt(sender[0]), sender[0] + "_" + msgId.split("_")[1], Base64.getEncoder().encodeToString(copy));
+        //System.out.println(Integer.parseInt(sender[0]) + "\n"  + msgId + "\n" + Base64.getEncoder().encodeToString(copy));
 
     }
 
@@ -270,8 +295,14 @@ public class Client {
         wrt.flush();
         JsonReader js = new JsonReader( new InputStreamReader( socket.getInputStream(), "UTF-8") );
         JsonElement data = new JsonParser().parse(js);
-        if (data.isJsonObject())
-            System.out.println(data.getAsJsonObject());
+        if (data.isJsonObject()) {
+            JsonObject obj = data.getAsJsonObject().get("result").getAsJsonObject().get("receipts").getAsJsonArray().get(0).getAsJsonObject();
+            if(!data.getAsJsonObject().get("result").getAsJsonObject().get("msg").getAsString().equals(obj.get("receipt").getAsString())) {
+                System.err.println("Wrong receipt");
+                throw new RuntimeException();
+            }   else
+                System.out.println("Receipts match");
+        }
 
 
     }
